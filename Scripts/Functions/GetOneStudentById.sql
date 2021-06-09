488519d5e879c32b8cdcf2843876eb40ce3ae9cb
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-06-09 08:56:12 -0300

    Initiated Layered archtecture implementation

--- Scripts/Functions/GetOneStudentById.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION public.GetOneStudentById(student_id INTEGER)
-RETURNS TABLE("Id" INTEGER, "FirstName" VARCHAR, "LastName" VARCHAR, "BirthDate" DATE)
-AS $$
-BEGIN
-    RETURN QUERY SELECT * FROM aluno WHERE aluno.id = student_id;
-END;
-$$
-LANGUAGE 'plpgsql';
-
