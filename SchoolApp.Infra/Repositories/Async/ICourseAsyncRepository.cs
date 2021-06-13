133c159bf428c9ca0e497e75641feea7b8091dec
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-06-13 07:39:16 -0300

    Refactor: Moved to .Repositories.Async Namespace and added Specific Entity Interfaces

---
+++ SchoolApp.Infra/Repositories/Async/ICourseAsyncRepository.cs
@@ -0,0 +1,10 @@
+using SchoolApp.Domain.Core.Models;
+using SchoolApp.Infra.Repositories.Shared;
+
+namespace SchoolApp.Infra.Repositories.Async
+{
+    public interface ICourseAsyncRepository : IAsyncRepository<Course>
+    {
+         
+    }
+}
