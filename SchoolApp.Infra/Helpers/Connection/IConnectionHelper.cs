68de8a765d1deb2c2c074a5147aad43bf182dce9
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-06-13 21:12:02 -0300

    Refactored SchoolApp.Domain.Core to SchoolApp.Domain and corrected infra interfaces.

--- SchoolApp.Infra/Helpers/Connection/IConnectionHelper.cs
+++
@@ -1,9 +0,0 @@
-using System.Data;
-
-namespace SchoolApp.Infra.Helpers.Connection
-{
-    public interface IConnectionHelper<T> where T : IDbConnection
-    {
-        T GetConnection(string connectionString);
-    }
-}
