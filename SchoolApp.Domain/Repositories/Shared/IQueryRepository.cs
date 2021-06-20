09190e46d3378a3a06c3d3234cfac2f8498310e0
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-06-20 11:59:55 -0300

    Fix:IQueryRepository GetByName should'nt be single

--- SchoolApp.Domain/Repositories/Shared/IQueryRepository.cs
+++ SchoolApp.Domain/Repositories/Shared/IQueryRepository.cs
@@ -7,7 +7,7 @@ namespace SchoolApp.Domain.Repositories.Shared
     public interface IQueryRepository<T> where T : class
     {
         Task<T> GetById(int id);
-        Task<T> GetByName(Name name);
+        Task<IEnumerable<T>> GetByName(Name name);
         Task<IEnumerable<T>> GetAll(int page, int rowCount);
         
     }
